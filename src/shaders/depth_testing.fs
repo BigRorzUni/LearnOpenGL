4f38c62bc8dade58c087d1d3eb6bba2268350189
Rory Edmonds <bigrorz@Rorys-MacBook-Air-3.local>
2024-11-06 13:36:08 +1300

    visualising depth buffer using frag shader

--- src/shaders/depth_testing.fs
+++ src/shaders/depth_testing.fs
@@ -1,11 +1,19 @@
 #version 330 core
-out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D texture1;
+out vec4 FragColor;
 
 void main()
-{    
-    FragColor = texture(texture1, TexCoords);
+{
+    float near = 0.1; 
+    float far = 100.0; 
+
+    float ndc = (2.0 * gl_FragCoord.z - 1.0);
+
+    float linearDepth = (2.0 * near * far) / (far + near - ndc * (far - near));
+
+    float depth = linearDepth / far;
+
+    FragColor = vec4(vec3(depth), 1.0);
 }
